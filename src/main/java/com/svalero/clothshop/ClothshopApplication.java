eaf1b1286b423204e91f75923000f52699b92512
Joserra2304 <cydogamer@gmail.com>
2023-01-13 12:51:56 +0100

    Proyecto inicial

---
+++ src/main/java/com/svalero/clothshop/ClothshopApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.clothshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/clothshop/ClothshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.clothshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
